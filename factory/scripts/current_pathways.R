994cc7d211a5473b1b27bcf7c7159aee1c14dcbd
Thibaut Jombart <thibautjombart@gmail.com>
2020-05-26 14:48:43 +0100

    data update; added automated selection of k

--- factory/scripts/current_pathways.R
+++ factory/scripts/current_pathways.R
@@ -1,8 +1,8 @@
 ## This file is generated automatically by `clean_latest_pathways`
 ## Do not edit it by hand!
 
-## This file was last updated on the: 2020-05-15 16:51:13
+## This file was last updated on the: 2020-05-22 15:46:08
 
 current_pathways <- here::here("data",
                                  "rds",
-                                 "pathways_2020-05-14.rds")
+                                 "pathways_2020-05-21.rds")
